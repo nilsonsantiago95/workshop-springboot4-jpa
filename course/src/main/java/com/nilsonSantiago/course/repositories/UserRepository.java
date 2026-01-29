68453c59600a3d79a562ef3c8a1e1039aa2385a1
Nilson Santiago <nilson.santiago19@gmail.com>
2026-01-29 12:37:14 -0300

    Service layer, component registration

--- course/src/main/java/com/nilsonSantiago/course/repositories/UserRepository.java
+++ course/src/main/java/com/nilsonSantiago/course/repositories/UserRepository.java
@@ -1,6 +1,7 @@
 package com.nilsonSantiago.course.repositories;
 
 import com.nilsonSantiago.course.entities.User;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
