caba0631010ee9d03d634a105b94091d11e1d11e
Nilson Santiago <nilson.santiago19@gmail.com>
2026-01-29 11:24:34 -0300

    JPA repository, dependency injection, database seeding

---
+++ course/src/main/java/com/nilsonSantiago/course/repositories/UserRepository.java
@@ -0,0 +1,8 @@
+package com.nilsonSantiago.course.repositories;
+
+import com.nilsonSantiago.course.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
