028d8f6d247a7ddb583244f661f8d9680c8ff679
Nilson Santiago <nilson.santiago19@gmail.com>
2026-01-30 00:43:00 -0300

    Order, Instant, ISO 8601 - Part 1

--- course/src/main/java/com/nilsonSantiago/course/entities/User.java
+++ course/src/main/java/com/nilsonSantiago/course/entities/User.java
@@ -3,6 +3,8 @@ package com.nilsonSantiago.course.entities;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 @Entity
@@ -19,8 +21,10 @@ public class User implements Serializable {
     private String phone;
     private String password;
 
-    public User() {
+    @OneToMany(mappedBy = "client")
+    private List<Order> orders = new ArrayList<>();
 
+    public User() {
     }
 
     public User(Long id, String name, String email, String phone, String password) {
@@ -29,6 +33,10 @@ public class User implements Serializable {
         this.email = email;
         this.phone = phone;
         this.password = password;
+    }
+
+    public List<Order> getOrders() {
+        return orders;
     }
 
     public Long getId() {
