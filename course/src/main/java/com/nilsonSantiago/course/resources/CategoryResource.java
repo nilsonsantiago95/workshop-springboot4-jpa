0be437b749f6c3eb7dbbe5cf2c099a210b3c3937
Nilson Santiago <nilson.santiago19@gmail.com>
2026-01-30 12:47:34 -0300

    Category

---
+++ course/src/main/java/com/nilsonSantiago/course/resources/CategoryResource.java
@@ -0,0 +1,34 @@
+package com.nilsonSantiago.course.resources;
+
+import com.nilsonSantiago.course.entities.Category;
+import com.nilsonSantiago.course.services.CategoryService;
+import jakarta.annotation.Resource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/categories")
+public class CategoryResource {
+
+    @Autowired
+    private CategoryService categoryService;
+
+    @GetMapping
+    public ResponseEntity<List<Category>> findAll() {
+        List<Category> categories = categoryService.findAll();
+        return ResponseEntity.ok().body(categories);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Category> findById(@PathVariable Long id) {
+        Category category = categoryService.findById(id);
+        return ResponseEntity.ok().body(category);
+    }
+
+}
---
+++ course/src/main/java/com/nilsonSantiago/course/services/CategoryService.java
@@ -0,0 +1,27 @@
+package com.nilsonSantiago.course.services;
+
+import com.nilsonSantiago.course.entities.Category;
+import com.nilsonSantiago.course.repositories.CategoryRepository;
+import com.nilsonSantiago.course.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class CategoryService {
+
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    public List<Category> findAll() {
+        return categoryRepository.findAll();
+    }
+
+    public Category findById(Long id) {
+        Optional<Category> category = categoryRepository.findById(id);
+        return category.get();
+    }
+
+}
