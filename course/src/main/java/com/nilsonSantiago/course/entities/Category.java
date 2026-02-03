b7dc24e011fb8bc84eca72eb571d5f58423c2bf9
Nilson Santiago <nilson.santiago19@gmail.com>
2026-02-03 00:53:23 -0300

    Many-to-many association with JoinTable

--- course/src/main/java/com/nilsonSantiago/course/entities/Category.java
+++ course/src/main/java/com/nilsonSantiago/course/entities/Category.java
@@ -1,5 +1,6 @@
 package com.nilsonSantiago.course.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
@@ -18,6 +19,9 @@ public class Category implements Serializable {
     private Long id;
     private String name;
 
+
+    @ManyToMany(mappedBy = "categories")
+    @JsonIgnore
     private Set<Product> products = new HashSet<>();
 
     public Category() {
