359d70d76a17e30cad34dacf6b1ad57929da6ecc
Nilson Santiago <nilson.santiago19@gmail.com>
2026-02-03 11:11:42 -0300

    OrderItem, many-to-many association with extra attributes - Part 1

--- course/src/main/java/com/nilsonSantiago/course/entities/Category.java
+++ course/src/main/java/com/nilsonSantiago/course/entities/Category.java
@@ -19,7 +19,6 @@ public class Category implements Serializable {
     private Long id;
     private String name;
 
-
     @ManyToMany(mappedBy = "categories")
     @JsonIgnore
     private Set<Product> products = new HashSet<>();
