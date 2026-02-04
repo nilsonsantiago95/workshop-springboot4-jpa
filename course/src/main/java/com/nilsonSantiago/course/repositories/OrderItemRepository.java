c8d9f01f259f79efe8fb7f61edba287ab010b45a
Nilson Santiago <nilson.santiago19@gmail.com>
2026-02-04 10:18:28 -0300

    Product-OrderItem one-to-many association

--- course/src/main/java/com/nilsonSantiago/course/repositories/OrderItemRepository.java
+++ course/src/main/java/com/nilsonSantiago/course/repositories/OrderItemRepository.java
@@ -1,7 +1,6 @@
 package com.nilsonSantiago.course.repositories;
 
 import com.nilsonSantiago.course.entities.OrderItem;
-import com.nilsonSantiago.course.entities.pk.OrderItemPK;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
