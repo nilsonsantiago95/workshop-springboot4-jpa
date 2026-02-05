f3785d401e09e26814f1ee75d308222f8003352c
Nilson Santiago <nilson.santiago19@gmail.com>
2026-02-05 11:18:39 -0300

    User delete

--- course/src/main/java/com/nilsonSantiago/course/resources/UserResource.java
+++ course/src/main/java/com/nilsonSantiago/course/resources/UserResource.java
@@ -41,4 +41,10 @@ public class UserResource {
         return ResponseEntity.created(uri).body(user);
     }
 
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        userService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
 }
