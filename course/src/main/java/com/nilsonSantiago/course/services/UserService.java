e6895ea5635f6a082e76245a2b420903b1cefd62
Nilson Santiago <nilson.santiago19@gmail.com>
2026-02-11 10:07:25 -0300

    Exception handling - update

--- course/src/main/java/com/nilsonSantiago/course/services/UserService.java
+++ course/src/main/java/com/nilsonSantiago/course/services/UserService.java
@@ -4,6 +4,7 @@ import com.nilsonSantiago.course.entities.User;
 import com.nilsonSantiago.course.repositories.UserRepository;
 import com.nilsonSantiago.course.services.exceptions.DatabaseException;
 import com.nilsonSantiago.course.services.exceptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
@@ -44,9 +45,13 @@ public class UserService {
     }
 
     public User update(Long id, User obj) {
-        User entity = userRepository.getReferenceById(id);
-        updateData(entity, obj);
-        return userRepository.save(entity);
+        try {
+            User entity = userRepository.getReferenceById(id);
+            updateData(entity, obj);
+            return userRepository.save(entity);
+        } catch(EntityNotFoundException e) {
+            throw new ResourceNotFoundException(id);
+        }
     }
 
     private void updateData(User entity, User obj) {
